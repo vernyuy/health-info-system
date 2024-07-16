2c014a556914e729356d7ed1b688e6b6434ccd74
fonchu <fonchu.e.venyuy@gmail.com>
2024-07-16 12:28:16 +0100

    added amplify

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,22 +1,11 @@
-import { defineBackend } from "@aws-amplify/backend";
-import { auth } from "./auth/resource.js";
-import { data, MODEL_ID, generateHaikuFunction } from "./data/resource";
-import { Effect, PolicyStatement } from "aws-cdk-lib/aws-iam";
-import { sayHello } from "./functions/bedrock-lambda-fn/resource";
-import { storage } from "./storage/resource.js"
+import { defineBackend } from '@aws-amplify/backend';
+import { auth } from './auth/resource';
+import { data } from './data/resource';
 
-export const backend = defineBackend({
+/**
+ * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
+ */
+defineBackend({
   auth,
   data,
-  storage,
-  generateHaikuFunction,
-  sayHello,
 });
-
-backend.generateHaikuFunction.resources.lambda.addToRolePolicy(
-  new PolicyStatement({
-    effect: Effect.ALLOW,
-    actions: ["bedrock:*"],
-    resources: [`arn:aws:bedrock:*::foundation-model/${MODEL_ID}`],
-  })
-);
--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,137 +1,17 @@
-import {
-  type ClientSchema,
-  a,
-  defineData,
-  defineFunction,
-} from "@aws-amplify/backend";
+import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
 
-export const MODEL_ID = "anthropic.claude-3-haiku-20240307-v1:0";
-
-export const generateHaikuFunction = defineFunction({
-  entry: "./generateHaiku.ts",
-  environment: {
-    MODEL_ID,
-  },
-});
-
+/*== STEP 1 ===============================================================
+The section below creates a Todo database table with a "content" field. Try
+adding a new "isDone" field as a boolean. The authorization rule below
+specifies that any unauthenticated user can "create", "read", "update", 
+and "delete" any "Todo" records.
+=========================================================================*/
 const schema = a.schema({
-  generateHaiku: a
-    .query()
-    .arguments({ prompt: a.string().required() })
-    .returns(a.string())
-    .authorization((allow) => [
-      allow.guest(),
-      allow.authenticated(),
-      allow.publicApiKey(),
-    ])
-    .handler(a.handler.function(generateHaikuFunction)),
-
-  Type: a.enum(["PRIVATE", "PUBLIC"]),
-  Location: a.customType({
-    lat: a.float(),
-    long: a.float(),
-  }),
-
-  User: a
+  Todo: a
     .model({
-      userId: a.id(),
-      cognitoId: a.string(),
-      username: a.string(),
-      phoneNumber: a.phone(),
-      email: a.email(),
-      firstName: a.string(),
-      lastName: a.string(),
+      content: a.string(),
     })
-    .authorization((allow) => [
-      allow.authenticated().to(["read"]),
-      allow.owner(),
-    ]),
-
-  Drug: a
-    .model({
-      drugId: a.id(),
-      name: a.string(),
-      description: a.string(),
-      imageUrl: a.string(),
-      pharmacies: a.hasMany("PharmacyDrug", "drugId"),
-    })
-    .authorization((allow) => [
-      allow.owner(),
-      allow.authenticated().to(["read"]),
-    ]),
-
-  Pharmacy: a
-    .model({
-      pharmacyId: a.id(),
-      name: a.string(),
-      description: a.string(),
-      location: a.ref("Location"),
-      imageUrl: a.string(),
-      drugs: a.hasMany("PharmacyDrug", "pharmacyId"),
-      healthCareProviderId: a.id(),
-      healthCareProvider: a.belongsTo(
-        "HealthCareProvider",
-        "healthCareProviderId"
-      ),
-    })
-    .authorization((allow) => [
-      allow.authenticated().to(["read"]),
-      allow.owner(),
-    ]),
-
-  PharmacyDrug: a
-    .model({
-      pharmacyId: a.id().required(),
-      drugId: a.id().required(),
-      pharmacy: a.belongsTo("Pharmacy", "pharmacyId"),
-      drug: a.belongsTo("Drug", "drugId"),
-    })
-    .authorization((allow) => [
-      allow.authenticated().to(["read"]),
-      allow.owner(),
-    ]),
-
-  Disease: a
-    .model({
-      diseaseId: a.id(),
-      name: a.string(),
-      description: a.string(),
-      symptoms: a.string().array(),
-      imageUrl: a.string(),
-      preventionTips: a.string().array(),
-    })
-    .authorization((allow) => [
-      allow.authenticated().to(["read"]),
-      allow.guest().to(["read"]),
-      allow.owner(),
-    ]),
-
-  HealthCareProvider: a
-    .model({
-      healthCareProviderId: a.id(),
-      name: a.string(),
-      description: a.string(),
-      imageUrl: a.string(),
-      location: a.ref("Location"),
-      type: a.ref("Type"),
-      pharmacy: a.hasOne("Pharmacy", "healthCareProviderId"),
-    })
-    .authorization((allow) => [
-      allow.authenticated().to(["read"]),
-      allow.owner(),
-    ]),
-
-  FirstAide: a
-    .model({
-      firstAideId: a.string(),
-      title: a.string(),
-      imageUrl: a.string(),
-      description: a.string(),
-    })
-    .authorization((allow) => [
-      allow.authenticated().to(["read"]),
-      allow.owner(),
-    ]),
+    .authorization((allow) => [allow.guest()]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
@@ -139,7 +19,35 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: "userPool",
-    apiKeyAuthorizationMode: { expiresInDays: 30 },
+    defaultAuthorizationMode: 'iam',
   },
 });
+
+/*== STEP 2 ===============================================================
+Go to your frontend source code. From your client-side code, generate a
+Data client to make CRUDL requests to your table. (THIS SNIPPET WILL ONLY
+WORK IN THE FRONTEND CODE FILE.)
+
+Using JavaScript or Next.js React Server Components, Middleware, Server 
+Actions or Pages Router? Review how to generate Data clients for those use
+cases: https://docs.amplify.aws/gen2/build-a-backend/data/connect-to-API/
+=========================================================================*/
+
+/*
+"use client"
+import { generateClient } from "aws-amplify/data";
+import type { Schema } from "@/amplify/data/resource";
+
+const client = generateClient<Schema>() // use this Data client for CRUDL requests
+*/
+
+/*== STEP 3 ===============================================================
+Fetch records from the database and use them in your frontend component.
+(THIS SNIPPET WILL ONLY WORK IN THE FRONTEND CODE FILE.)
+=========================================================================*/
+
+/* For example, in a React component, you can use this snippet in your
+  function's RETURN statement */
+// const { data: todos } = await client.models.Todo.list()
+
+// return <ul>{todos.map(todo => <li key={todo.id}>{todo.content}</li>)}</ul>
